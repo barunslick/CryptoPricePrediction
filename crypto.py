d6d6cd176cf18b25d484eea55728c22d38d1f7d8
barunslick <barunpradhan987@gmail.com>
2020-04-05 16:29:36 +0545

    Final some changes

--- crypto.py
+++ crypto.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
+
+# In[1]:
+
+
 import numpy as np
 import pandas as pd
 import tensorflow as tf
@@ -7,21 +11,62 @@ import random
 from sklearn import preprocessing
 import collections
 
+
+# In[2]:
+
+
 look_back_period = 30
 future_look_period = 3
 
-data = pd.read_csv('ETH-USD.csv', names='time low high open close volume'.split())
+
+# In[3]:
+
+
+data = pd.read_csv('ETH-USD.csv', names='time low high open close volume'.split()) 
+
+
+# In[4]:
+
+
 data.set_index('time', inplace = True)
 
+
+# In[5]:
+
+
 data.sort_index(inplace = True)
-data = data[['close','volume']]
+
+
+# In[6]:
+
+
+data = data[['close','volume']] 
+
+
+# In[7]:
+
+
 data['future_value'] = data['close'].shift(-future_look_period)
 data.dropna(inplace = True)
+
+
+# In[8]:
+
+
 data['target'] = data.apply(lambda row : 1 if (row['future_value'] >  row['close']) else 0, axis = 1) 
+
+
+# In[9]:
+
+
 trainSet, testSet,_ = np.split(data,[int(0.95*len(data)),len(data)])
 
+
+# In[10]:
+
+
 def preprocess_data(data_pre):
-    data_pre.drop('future_value',axis = 1)
+    data_pre.drop('future_value',axis = 1,inplace=True)
     for col in data_pre.columns:
         if col != 'target':
             data_pre[col] = data_pre[col].pct_change()
@@ -54,24 +99,53 @@ def preprocess_data(data_pre):
         decision.append(target)
     return np.array(inputs), np.array(decision)
 
+
+# In[11]:
+
+
 trainInputs, trainTargets = preprocess_data(trainSet)
 testInputs, testTargets = preprocess_data(testSet)
 
+
+# In[12]:
+
+
 model = tf.keras.Sequential([
-        tf.keras.layers.LSTM(128, activation='tanh', recurrent_activation='sigmoid',return_sequences=True,dropout=0.2,input_shape = trainInputs.shape[1:]),
-        tf.keras.layers.LSTM(128, activation='tanh', recurrent_activation='sigmoid',return_sequences=True,dropout=0.2),
-        tf.keras.layers.LSTM(128, activation='tanh', recurrent_activation='sigmoid',return_sequences=False,dropout=0.2),
-        tf.keras.layers.Dense(64,activation='relu'),
-        tf.keras.layers.Dense(32,activation='relu'),
+        tf.keras.layers.LSTM(32, activation='tanh', recurrent_activation='sigmoid',return_sequences=True,dropout=0.2,input_shape = trainInputs.shape[1:]),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LSTM(32, activation='tanh', recurrent_activation='sigmoid',return_sequences=True,dropout=0.2),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.LSTM(32, activation='tanh', recurrent_activation='sigmoid',return_sequences=False,dropout=0.2),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Dense(16,activation='relu'),
+        tf.keras.layers.Dropout(0.1),
         tf.keras.layers.Dense(2,activation='softmax')
 ])
+
+
+# In[13]:
+
 
 model.compile(loss='sparse_categorical_crossentropy',
               optmizer='adam',
               metrics=['accuracy']
              )
 
-model.fit(trainInputs, trainTargets,validation_data=(testInputs, testTargets),epochs=5,batch_size=40)
+
+# In[14]:
+
+
+model.fit(trainInputs, trainTargets,validation_data=(testInputs, testTargets),epochs=10,batch_size=20)
+
+
+# In[17]:
+
 
 model.save('cryptoPrediction.model')
+
+
+# In[ ]:
+
+
+get_ipython().system('jupyter nbconvert')
 
